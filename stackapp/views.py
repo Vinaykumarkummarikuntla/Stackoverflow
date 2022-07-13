5df0616d7d3f4d5e660a1545e005f5048cced662
Vinaykumarkummarikuntla <kvk.9618@gmail.com>
2022-07-13 17:03:14 +0530

    code

--- stackapp/views.py
+++ stackapp/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 def display(request):
     s='<h1>Hello Welcome to Heroku Server!!!</h1>'
-    return HttpResponse(s)
+    return render(request,'Home.html')
