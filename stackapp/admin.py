3ed8e8f338704bc6d1e5701f07214aee38c678ab
Vinaykumarkummarikuntla <kvk.9618@gmail.com>
2022-07-13 15:52:15 +0530

    code

--- stackapp/admin.py
+++ stackapp/admin.py
@@ -1,3 +1,8 @@
+from atexit import register
 from django.contrib import admin
+from stackapp.models import Question, Comment, user
 
 # Register your models here.
+admin.site.register(Question)
+admin.site.register(Comment)
+admin.site.register(user)
